e169c1494b44284e70eb0ef01a8d1fdf16904ace
Alvaro David Ruidiaz Sosa <adruidiaz@unicesar.edu.co>
2026-03-01 22:35:43 -0500

    feat: crear clase Estudiante con atributos

---
+++ src/sistemaacademico/estudiante.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+/**
+ *
+ * @author hp
+ */
+public class estudiante {
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private int edad; 
+    private int semestre;
+     
+}
