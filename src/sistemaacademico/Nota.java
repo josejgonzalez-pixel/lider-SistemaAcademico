29f28928404c6271f60a991ef69e28bfd64a2c71
candreapadilla <candreapadilla@unicesar.edu.co>
2026-03-02 19:46:22 -0500

    feat: crear clase Nota con atributos

---
+++ src/sistemaacademico/Nota.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+/**
+ *
+ * @author Camil
+ */
+public class Nota {
+    
+    private String codigoEstudiante;
+    private String codigoAsignatura;
+    private double valor;
+    
+}
