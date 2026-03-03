446c9ffe5c32ddd55760ff65212d4616d5df9c90
candreapadilla <candreapadilla@unicesar.edu.co>
2026-03-02 19:33:41 -0500

    feat: crear clase Asignatura con atributos

---
+++ src/sistemaacademico/Asignatura.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+/**
+ *
+ * @author Camil
+ */
+public class Asignatura {
+    
+    private String codigo;
+    private String nombre;
+    private int creditos;
+    
+}
