db4f26c946dc74efd4b81b21d0ce1a1cf4ef9b30
candreapadilla <candreapadilla@unicesar.edu.co>
2026-03-02 19:40:04 -0500

    feat: agregar encapsulamiento a clase Asignatura

--- src/sistemaacademico/Asignatura.java
+++ src/sistemaacademico/Asignatura.java
@@ -13,5 +13,36 @@ public class Asignatura {
     private String codigo;
     private String nombre;
     private int creditos;
+
+    public Asignatura(String codigo, String nombre, int creditos) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+    
     
 }
